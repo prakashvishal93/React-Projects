7d40747bf0d713ee62264d64a8a6369e4acf2e3f
Vishal Prakash <vishalprakash@Vishals-MacBook-Air.local>
2024-05-02 22:20:20 +0530

    building frontend of the tomato

--- 05-PasswordGenerator/src/App.jsx
+++ 05-PasswordGenerator/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
 
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <>
+ 
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className='text-3xl font-bold mb-2 text-center'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text"value={password} placeholder='Password' className='outline-none w-full py-1 px-3  read-only '/>
+          <button className='outline-none bg-blue-500 text-white px-3 py-0.5 shrink-0'>Copy</button>
+    
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range"name='' id=''min={6} max={100} value={length} className='cursor-pointer' onChange={(e) => setLength(e.target.value )}/>
+            <label htmlFor="length">Length: {length}</label>
+          </div>
+          
+          <div className='flex items-center gap-x-1'></div>
+          <input type="checkbox"
+          defaultChecked = {numberAllowed}
+          onChange={() =>{
+            setNumberAllowed((prev)=> !prev )
+          }}
+           name='' id=''   />
+
+           <label htmlFor="number">Numbers</label>
+
+          
+        </div>
+ 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+   </>
   )
 }
 
---
+++ Tomato/frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore Our Menu</h1>
+        <p className='explore-menu-text'></p>
+      
+    </div>
+  )
+}
+
+export default ExploreMenu
