882298b9f961f2e68707d66ecf33b7b46ebf5546
Vishal Prakash <vishalprakash@Vishals-MacBook-Air.local>
2024-05-07 10:53:35 +0530

    adding responsiveness

--- Tomato/frontend/src/App.jsx
+++ Tomato/frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
-import Home from './pages/Home/Home'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-
-      <Navbar />
-      <Routes>
-        <Route path='/' element = {<Home />} /> 
-        <Route path='/cart' element = {<Cart />} /> 
-        <Route path='/order' element = {<PlaceOrder />} /> 
-      </Routes>
-
-      
-    </div>
-  )
-}
-
-export default App
+export default App;
