8e9a328739a6cf0186736f5b612cbe008efa315a
Vishal Prakash <vishalprakash@Vishals-MacBook-Air.local>
2024-05-08 22:29:45 +0530

    added database logic and connected to the atlas

--- Tomato/backend/server.js
+++ Tomato/backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
-import cors from 'cors '
+import cors from 'cors'
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
 
 
 
@@ -10,3 +12,25 @@ const PORT = 4000;
 
 //middlewares
 app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use('/images',express.static('uploads '))
+
+
+
+app.get('/', (req,res)=>{
+    res.send("Kuch bhi return karde")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
+
+// mongodb+srv://admin:admin123@cluster0.sxvmywr.mongodb.net/?
+ 
