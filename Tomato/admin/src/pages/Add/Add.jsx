17545b6de7f941e0ed1ab1653889b60790e02a6e
Vishal Prakash <vishalprakash@Vishals-MacBook-Air.local>
2024-05-10 00:58:57 +0530

    admin panel created

--- Tomato/admin/src/pages/Add/Add.jsx
+++ Tomato/admin/src/pages/Add/Add.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
 import { useState } from "react";
+import axios from 'axios'
+import { toast } from "react-toastify";
 
-const Add = () => {
+const Add = ({url}) => {
+
+
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -21,6 +25,29 @@ const Add = () => {
 
   const onSubmitHandler = async (event) =>{
     event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name",data.name)
+    formData.append("description",data.description)
+    formData.append("price",Number(data.price))
+    formData.append("category",data.category)
+    formData.append("image",image)
+    const response = await axios.post(`${url}/api/food/add`,formData);
+    if(response.data.success){
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+      })
+      setImage(false)
+      toast.success(response.data.message)
+
+    } 
+    else{
+      toast.error(response.data.message)
+
+    }
 
   }
 
